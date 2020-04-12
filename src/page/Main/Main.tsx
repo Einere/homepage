e23aaedd97ae909dcfa0c9b6e4742db92037d6a0
Einere <kjwsx23@gmail.com>
2020-04-12 15:08:04 +0900

    Init: initialize project
    Signed-off-by: Einere <kjwsx23@gmail.com>

---
+++ src/page/Main/Main.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {FunctionComponent} from 'react';
+
+export const Main: FunctionComponent = function () {
+    return (
+        <>
+            main
+        </>
+    );
+};
