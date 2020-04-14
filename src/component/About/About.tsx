4dc5bf06bc4cb17ed73972d8c5854aa8ba02ef89
Einere <kjwsx23@gmail.com>
2020-04-14 20:54:40 +0900

    feat: implement About
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- src/component/About/About.tsx
+++ src/component/About/About.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import {FunctionComponent} from 'react';
 import {SectionStyle} from "./Section.style";
-import selfie from '../../asset/image/selfie.jpg';
 import {Title} from "../Title/Title";
+import {Profile} from "../Profile/Profile";
 
 export const About: FunctionComponent = function () {
     const title = 'ABOUT';
@@ -10,56 +10,7 @@ export const About: FunctionComponent = function () {
     return (
         <SectionStyle>
             <Title title={title}/>
-            <div className="picture">
-                <img src={selfie} alt="Einere's selfie"/>
-                <div className="name">Einere</div>
-                <div className="description">STUDENT</div>
-            </div>
-            <div className="text">
-                <div className="intro">
-                    web developer
-                </div>
-                <ul>
-                    <li>
-                        <div className="info">
-                            <i className="far fa-calendar-alt"/> dd MM, yyyy
-                        </div>
-
-                    </li>
-                    <li>
-                        <div className="info">
-                            <i className="fa fa-phone"/> +82 10-xxxx-xxxx
-                        </div>
-                    </li>
-                    <li>
-                        <div className="info">
-                            <i className="fa fa-envelope"/> kjwsx23@gmail.com
-                        </div>
-                    </li>
-                    <li>
-                        <div className="info">
-                            <i className="fa fa-home"/> Seoul, Republic of Korea
-                        </div>
-                    </li>
-                </ul>
-                <div className="sns">
-                    <a target="_blank" rel="noopener noreferrer" href="https://facebook.com/hyungjun.choi.146">
-                        <i className="fab fa-facebook-f"/>
-                    </a>
-                    <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/alban_arthuan/">
-                        <i className="fab fa-instagram"/>
-                    </a>
-                </div>
-            </div>
-            <div className="history">
-                <div className="history-title">
-                    HISTORY
-                </div>
-                <p>2013.03 - 광운대학교 입학</p>
-                <p>2014.10 ~ 2016.09 - 대한민국 해군 복무</p>
-                <p>2017.09 ~ 12 - 융합 KWIX 수행</p>
-                <p>2018.07 ~ 08 - 럭스로보 웹 개발 인턴 근무</p>
-            </div>
+            <Profile/>
         </SectionStyle>
     );
 };
--- src/component/About/Section.style.ts
+++ src/component/About/Section.style.ts
@@ -1,5 +1,4 @@
 import styled from "styled-components";
 
 export const SectionStyle = styled.section`
-  padding: 5rem 0;
 `;
