2cd58140c5008ad43c722d4f205d080a23e92f82
Einere <kjwsx23@gmail.com>
2020-04-17 14:59:30 +0900

    fix: fix type for IconButton
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- src/component/IconButton/IconButton.tsx
+++ src/component/IconButton/IconButton.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import {FunctionComponent} from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {IconButtonStyle} from "./IconButton.style";
+import {IconProp} from "@fortawesome/fontawesome-svg-core";
 
 interface Prop {
     href: string;
-    icon: any;
+    icon: IconProp;
 }
 
 export const IconButton: FunctionComponent<Prop> = function ({href, icon}) {
