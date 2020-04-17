a21575e79a7545897b3c37fcc1fb4760049acc68
Einere <kjwsx23@gmail.com>
2020-04-17 13:08:12 +0900

    style : add margin
    화면 테두리와 떨어트리기 위해 마진을 추가함.
    모바일 환경을 고려하여 width변경을 추가함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- src/component/About/Section.style.ts
+++ src/component/About/Section.style.ts
@@ -1,4 +1,10 @@
 import styled from "styled-components";
 
 export const SectionStyle = styled.section`
+  width: 80%;
+  margin: 0 auto;
+  
+  @media(max-width: 992px) {
+    width: 100%;
+  }
 `;
