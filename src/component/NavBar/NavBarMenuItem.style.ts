5de980c529551ce0a4949fe41ad921c28abbbf00
Einere <kjwsx23@gmail.com>
2020-04-17 19:19:24 +0900

    typo: modify lower case for menu button
    네비게이션 바의 메뉴를 눌렀을 때 스크롤 기능을 추가하기 위해, 기존의 대문자 텍스트를 소문자로 변경함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- src/component/NavBar/NavBarMenuItem.style.ts
+++ src/component/NavBar/NavBarMenuItem.style.ts
@@ -7,6 +7,7 @@ export const NavBarMenuItemStyle = styled.div`
   line-height: 5rem;
   transition: all 0.5s ease;
   text-align: center;
+  text-transform: uppercase;
   
   &:hover {
     background-color: rgba(0, 0, 0, 0.4);
