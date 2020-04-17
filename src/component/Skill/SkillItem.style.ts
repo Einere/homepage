167cd418566a0833496bde653b79ccb18d72c62b
Einere <kjwsx23@gmail.com>
2020-04-17 15:00:04 +0900

    feat: implements SkillItem
    Signed-off-by: Einere <kjwsx23@gmail.com>

---
+++ src/component/Skill/SkillItem.style.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const SkillItemStyle = styled.div`
+  background-color: white;
+  padding: 1rem;
+  margin: 2rem;
+  transition: all 0.5s ease;
+
+  &:hover {
+    transform: scale(1.02);
+    box-shadow: 0 6px 20px 0 rgba(0, 0, 0, 0.2);
+  }
+
+  & > .service {
+    font-size: 32px;
+    font-weight: bold;
+    padding: 10px 0;
+  }
+
+  & > .content {
+    font-size: 16px;
+    font-family: 'Noto Sans KR', sans-serif;
+    letter-spacing: 1px;
+    padding: 10px 0;
+  }
+`;
