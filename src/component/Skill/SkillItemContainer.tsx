433368aaeb037c3e946c71f2a193778673a66753
Einere <kjwsx23@gmail.com>
2020-04-17 15:01:11 +0900

    feat: implement SkillItemContainer
    Signed-off-by: Einere <kjwsx23@gmail.com>

---
+++ src/component/Skill/SkillItemContainer.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import {FunctionComponent} from 'react';
+import {SkillItemContainerStyle} from "./SkillItemContainerStyle";
+import {SkillItem} from "./SkillItem";
+import {faJsSquare, faNodeJs, faReact, faVuejs} from "@fortawesome/free-brands-svg-icons";
+import {IconProp} from "@fortawesome/fontawesome-svg-core";
+
+export interface SkillData {
+    icons: IconProp[];
+    category: string;
+    contents: string[];
+}
+
+const skills: SkillData[] = [
+    {
+        icons: [faJsSquare],
+        category: "Language",
+        contents: ["Java", "JavaScript", "TypeScript", "functional JS"]
+    },
+    {
+        icons: [faReact, faVuejs],
+        category: "FrontEnd",
+        contents: ["React", "Vue.js"]
+    },
+    {
+        icons: [faNodeJs],
+        category: "BackEnd",
+        contents: ["Node.js", "Express"]
+    },
+];
+
+export const SkillItemContainer: FunctionComponent = function () {
+    const SkillItems = skills.map(
+        ({icons, category, contents}, index) => <SkillItem key={index} icons={icons}
+                                                          category={category}
+                                                          contents={contents}/>
+    );
+
+    return (
+        <SkillItemContainerStyle>
+            {SkillItems}
+        </SkillItemContainerStyle>
+    );
+};
