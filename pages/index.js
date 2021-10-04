a9ae03495d3a0d418751952fdf3f466224f28f1e
Einere <kjwsx23@gmail.com>
2021-10-04 19:42:50 +0900

    env: update .gitignore, commands

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      Hello World.{' '}
+      Hello World. Einere
       <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
         <a>About</a>
       </Link>
