357e85f051fbd4716bc53927b58a4bbee553f2cd
Einere <kjwsx23@gmail.com>
2021-10-31 20:46:56 +0900

    env: modify static asset uri

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,11 @@ class MyDocument extends Document {
     return (
       <Html lang="ko">
         <Head>
-          <link rel="preload" href="/fonts/FZuanSu/FZuanSu.ttf" as="font" />
+          <link
+            rel="preload"
+            href="https://firebasestorage.googleapis.com/v0/b/homepage-229db.appspot.com/o/static%2FFZuanSu.ttf?alt=media&token=d4a5f655-5cf3-4e1d-9a3b-2b1c07d5ce1e"
+            as="font"
+          />
         </Head>
         <body>
           <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,8 +14,14 @@ const Home: NextPage = () => {
           playsInline // for mobile
           className={styles.bannerVideo}
         >
-          <source src="/videos/sea.mp4" type={"video/mp4"} />
-          <source src="/videos/sea.webm" type={"video/webm"} />
+          <source
+            src="https://firebasestorage.googleapis.com/v0/b/homepage-229db.appspot.com/o/static%2Fsea.mp4?alt=media&token=3ec9816f-0665-4c58-86eb-11d233678c56"
+            type={"video/mp4"}
+          />
+          <source
+            src="https://firebasestorage.googleapis.com/v0/b/homepage-229db.appspot.com/o/static%2Fsea.webm?alt=media&token=04a89de7-d913-4bf8-82ac-6fbef57ce2bc"
+            type={"video/webm"}
+          />
           Oops! Your browser doesn't support video. 😢
         </video>
         <span className={styles.bannerTitle}>香格里拉</span>
