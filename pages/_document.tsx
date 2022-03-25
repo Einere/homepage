5540750e057ef09888cf3860fe67e6c077cfb22e
Einere <kjwsx23@gmail.com>
2022-03-25 23:03:53 +0900

    env: modify font settings
    - 기존 코드는 FZuanSu 폰트가 깔려있는 노트북에서만 정상작동하고, 모바일 등 외부 기기에서는 제대로 폰트가 설정되지 않음.
    이를 해결하기 위해, css font-face를 이용해 해결함

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,13 +4,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="ko">
-        <Head>
-          <link
-            rel="preload"
-            href="https://firebasestorage.googleapis.com/v0/b/homepage-229db.appspot.com/o/static%2FFZuanSu.ttf?alt=media&token=d4a5f655-5cf3-4e1d-9a3b-2b1c07d5ce1e"
-            as="font"
-          />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
