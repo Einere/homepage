6d702255a6f2ea72c064a38db7994fa184817824
Einere <kjwsx23@gmail.com>
2025-03-18 21:12:43 +0900

    feat: 여러가지 업데이트
    1. env.d.ts 추가
    2. post -> record 워딩 변경
    3. 메타태그 타이틀 동작 방식 살짝 수정 (템플릿 방식으로)
    4. 각종 로딩 UI 추가
    5. notion api를 추가해서 노션 글 목록 가져오기 + 기록들 페이지 수정
    6. 가져온 노션 데이터를 이용해 글을 렌더링 하는 라이브러리 적용 + 기록 페이지 구현
    7. 랜딩 페이지에 애매한 이미지 일단 제거

---
+++ src/app/records/page.tsx
@@ -0,0 +1,14 @@
+import RecordsPageSkeleton from "@/app/records/loading";
+import { Suspense } from "react";
+import { Records } from "@/app/components/Records";
+
+export default async function RecordsPage() {
+  return (
+    <main>
+      <h1 className="text-4xl">여정의 발자취를 작은 기록들로 남겨봅니다. 🐾</h1>
+      <Suspense fallback={<RecordsPageSkeleton />}>
+        <Records />
+      </Suspense>
+    </main>
+  );
+}
