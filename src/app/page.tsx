b63ad576e0042fddf8792c8d3b6a2567e5759174
Einere <kjwsx23@gmail.com>
2025-03-21 15:07:26 +0900

    style: 파비콘 및 애플 아이콘 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import RecentRecordsSection from "@/app/components/RecentRecordsSection";
 export default function HomePage() {
   return (
     <main className="min-h-screen">
-      <h1 className="text-main-100 bg-[url(/image-from-rawpixel-id-13194800.jpg)] bg-fixed px-6 py-24 text-center text-5xl font-extrabold break-keep [background-position-x:center]">
+      <h1 className="text-main-100 bg-[url(/image-from-rawpixel-id-13194800.jpg)] bg-cover bg-fixed px-6 py-24 text-center text-5xl font-extrabold break-keep [background-position-x:center]">
         이 공간을 찾아주신 당신을 환영합니다.
       </h1>
       <div className="mx-auto max-w-[768px] py-20">
