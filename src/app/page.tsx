3e6c60cf44f194f3b0c1b77206e457b8a5fd8318
Einere <kjwsx23@gmail.com>
2025-04-01 00:37:21 +0900

    style: 랜딩 페이지 이미지 및 스타일 변경
    -

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { Suspense } from "react";
 export default function HomePage() {
   return (
     <main className="min-h-screen">
-      <h1 className="text-main-100 bg-[url(/image-from-rawpixel-id-13194800.jpg)] bg-cover bg-fixed px-6 py-24 text-center text-5xl break-keep [background-position-x:center]">
+      <h1 className="text-main-100 [background-position-x]:center mx-auto max-h-[673px] bg-[url(/blue_and_pink_flowers.jpeg)] bg-fixed px-6 py-24 text-center text-5xl break-keep lg:bg-contain">
         이 공간을 찾아주신 당신을 환영합니다.
       </h1>
       <div className="mx-auto max-w-[768px] py-20">
