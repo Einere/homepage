8a48c3bbef5ae6a435b0b62278765ffd06d45ff6
Einere <kjwsx23@gmail.com>
2025-03-31 22:39:08 +0900

    style: 서치 페이지 기본 스타일 적용
    - 간격 및 기본 높이/너비 지정

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,7 @@
 export default function SearchPage() {
   return (
-    <main>
-      <h1 className="">찾고 싶은게 무엇인가요? 🔍</h1>
+    <main className="mx-auto min-h-screen max-w-[768px]">
+      <h1 className="mt-16 mb-6">찾고 싶은게 무엇인가요? 🔍</h1>
       <p>돋보기를 잃어버렸어요. 조만간 돋보기를 구해놓도록 하겠습니다.</p>
     </main>
   );
