42a073731c2612d42d9a5ae8780501aa6c2e7988
Einere <kjwsx23@gmail.com>
2025-03-31 22:53:55 +0900

    chore: 소개 페이지 슬로건에 대한 보충 내용 추가
    - 추가로 h3 기본 스타일 적용

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -13,7 +13,6 @@ export default function Profile() {
       />
       <div>
         <h2>최형준</h2>
-        <p className="pt-4">&#34;Einere&#34;라는 닉네임을 주로 사용합니다.</p>
         <blockquote>
           <p>제품을 만드는, 지속가능한 웹 개발자를 지향합니다.</p>
         </blockquote>
@@ -23,27 +22,36 @@ export default function Profile() {
           깨달았습니다. 이를 위해 오너십을 가지고 동료들과 다양한 의견을 주고
           받는 등, 제품을 만들어 나가는 여정에 적극적으로 동참합니다.
         </p>
-        <p>
+        <p className="pb-4">
           개발 산업은 그 어느 분야보다도 빠르게 변화합니다. 이 변화 속에서
           뒤처지지 않기 위해, 새로운 기술과 흐름을 민감하게 받아들이고
           지속적으로 배우려는 태도가 중요하다고 생각합니다. 꾸준히 개발자로서
           성장하기 위해, 관심을 가지고 탐구하며, 배움을 멈추지 않으려고
           노력합니다.
         </p>
-        <ul className="mt-4 flex flex-col gap-2">
+        <h3 className="pb-4">저에 대해 더 자세히 알고 싶으신가요?</h3>
+        <ul className="flex flex-col gap-2">
           <li>
             <Link href="mailto:kjwsx23@einere.me" target="_blank">
-              📧E-mail
+              📧 E-mail
             </Link>
           </li>
           <li>
             <Link href="https://github.com/Einere" target="_blank">
-              🐙GitHub
+              🐙 GitHub
             </Link>
           </li>
           <li>
             <Link href="https://velog.io/@kjwsx23" target="_blank">
-              ☕️Velog
+              ☕️ Velog
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="https://www.linkedin.com/in/hyungjun-choi-760737258"
+              target="_blank"
+            >
+              🔗 Linked In
             </Link>
           </li>
         </ul>
