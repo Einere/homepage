de9c7558d01208955d9e5421e668401e70bcab57
Einere <kjwsx23@gmail.com>
2025-04-07 22:46:13 +0900

    feat: not-found.tsx 페이지 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import Profile from "@/app/components/Profile";
+
+export default function NotFoundPage() {
+  return (
+    <main className="mx-auto min-h-screen max-w-[768px] px-4 lg:px-0">
+      <p className="mt-16 mb-6">
+        앗! 주소가 잘못된 것 같아요. 😓
+        <br />
+        주소를 한 번 확인해주세요!
+      </p>
+    </main>
+  );
+}
