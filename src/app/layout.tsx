cc28b23e272549801180c51b8529be9152c02ff0
Einere <kjwsx23@gmail.com>
2025-04-07 17:42:31 +0900

    chore: 스타일링 관련 파일을 styles 하위로 옮김

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "./styles/globals.css";
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
 
