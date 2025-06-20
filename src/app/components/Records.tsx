66b104a989ffe9f422fcd60056f05cc51a225184
Einere <kjwsx23@gmail.com>
2025-06-20 23:12:31 +0900

    feat: 일단 야매로 페이지네이션 구현
    - 각 태그 별 동적 페이지네이션 기능 구현
    - 인메모리로 커서맵을 관리하는게 맞는건가 싶지만..
    - 노션 API가 index 기반 쿼리를 지원하지 않음..
    - 어쩔 수 없이 linked list 마냥 포인터 타고 가면서 일일이 계산하거나, 그냥 동적으로 페이지 렌더링 하거나..

--- src/app/components/Records.tsx
+++ src/app/components/Records.tsx
@@ -9,60 +9,106 @@ import {
   NOTION_BLOG_RECORDS_PROPERTIES,
 } from "@/app/utils/notionUtils";
 import RecordCard from "@/app/components/RecordCard";
+import Link from "next/link";
 
 type RecordsProps = {
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
-export async function Records(params: RecordsProps) {
-  const tagFilter = getFilter(params.searchParams.tag);
+const PAGE_SIZE = 5;
+const ALL_TAG = "ALL";
+const cursorMap: Record<string, Record<string, string | undefined>> = {
+  [ALL_TAG]: {
+    "0": undefined,
+  },
+};
 
-  const body = tagFilter
-    ? await getRecordsFromNotion({
-        filter: tagFilter,
-      })
-    : await getRecordsFromNotion();
-  const { results } = body;
+export async function Records(params: RecordsProps) {
+  const tag = getSearchParam(params.searchParams.tag);
+  const page = getSearchParam(params.searchParams.page) ?? "0";
+  const currentPage = parseInt(page, 10);
+
+  let results = [];
+
+  if (tag && !cursorMap[tag]) {
+    Object.assign(cursorMap, { [tag]: { "0": undefined } });
+  }
+
+  const response = await getRecordsFromNotion({
+    filter: tag ? getTagFilter(tag) : undefined,
+    page_size: PAGE_SIZE,
+    start_cursor: cursorMap[tag ?? ALL_TAG][currentPage],
+  });
+
+  const { has_more, next_cursor } = response;
+  results = response.results;
+
+  // 다음 페이지 커서 설정
+  if (has_more && next_cursor) {
+    const nextPage = currentPage + 1;
+
+    Object.assign(cursorMap[tag ?? ALL_TAG], {
+      [nextPage.toString()]: next_cursor,
+    });
+  }
 
   if (results.length === 0) {
     return <p>보여드릴 기록이 없어요. 🫥</p>;
   }
 
   return (
-    /* TODO: 페이지네이션 도입하기 */
-    <ul>
-      {results.filter(isPageObjectResponse).map((record) => {
-        const title = getTitleFromQueryPageObjectResponse(record) ?? "";
-        const description = getDescriptionFromPageObjectResponse(record) ?? "";
-        const id = getIdFromPageObjectResponse(record);
-        const publishedDate = getPublishedDateFromPageObjectResponse(record);
-        const tags = getTagsFromPageObjectResponse(record);
+    <>
+      <ul>
+        {results.filter(isPageObjectResponse).map((record) => {
+          const title = getTitleFromQueryPageObjectResponse(record) ?? "";
+          const description =
+            getDescriptionFromPageObjectResponse(record) ?? "";
+          const id = getIdFromPageObjectResponse(record);
+          const publishedDate = getPublishedDateFromPageObjectResponse(record);
+          const tags = getTagsFromPageObjectResponse(record);
 
-        return (
-          <li key={id} className="pb-4">
-            <RecordCard
-              id={id}
-              title={title}
-              description={description}
-              publishedDate={publishedDate}
-              tags={tags}
-            />
-          </li>
-        );
-      })}
-    </ul>
+          return (
+            <li key={id} className="pb-4">
+              <RecordCard
+                id={id}
+                title={title}
+                description={description}
+                publishedDate={publishedDate}
+                tags={tags}
+              />
+            </li>
+          );
+        })}
+      </ul>
+      <ol className="flex justify-center gap-2 pb-16">
+        {Object.keys(cursorMap[tag ?? ALL_TAG]).map((page) => (
+          <Link
+            key={page}
+            href={{
+              query: { tag: tag ? tag : undefined, page: page },
+            }}
+          >
+            {page}
+          </Link>
+        ))}
+      </ol>
+    </>
   );
 }
 
-function getFilter(searchParamValue: string | string[] | undefined) {
+function getSearchParam(searchParamValue: string | string[] | undefined) {
   if (searchParamValue === undefined || searchParamValue instanceof Array) {
     return null;
   }
 
+  return searchParamValue;
+}
+
+function getTagFilter(tag: string) {
   return {
     property: NOTION_BLOG_RECORDS_PROPERTIES.TAGS,
     multi_select: {
-      contains: searchParamValue,
+      contains: tag,
     },
   };
 }
